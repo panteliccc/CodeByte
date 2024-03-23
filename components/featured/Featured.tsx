427d9ad0f02b516cf353091e350c4ee480ba3809
Nikola Pantelic <pantelicnikolaa@gamail.com>
2024-03-23 17:12:59 +0100

    update featured post

--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -24,7 +24,7 @@ interface Data {
 const getData = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/featured`, {
     method: "GET",
-    cache: "no-cache",
+    cache: "no-store",
   });
 
   if (!res.ok) {
